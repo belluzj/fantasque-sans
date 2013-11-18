3696a233473cbe7717867516108fda007a32e0fb
Jany Belluz <Jany.Belluz@ensimag.imag.fr>
2013-11-18 17:29:14 +0100

    make images fit better in Github's display

--- zip.sh
+++ zip.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-zip CosmicSansNeueMono.zip OFL.txt README CosmicSansNeueMono.ttf CosmicSansNeueMonoBold.ttf OTF/CosmicSansNeueMonoBold.otf OTF/CosmicSansNeueMono.otf Sources/CosmicSansNeueMono.sfd Sources/CosmicSansNeueMonoBold.sfd
+zip CosmicSansNeueMono.zip OFL.txt README.md CosmicSansNeueMono.ttf CosmicSansNeueMonoBold.ttf OTF/CosmicSansNeueMonoBold.otf OTF/CosmicSansNeueMono.otf Sources/CosmicSansNeueMono.sfd Sources/CosmicSansNeueMonoBold.sfd
 
 
